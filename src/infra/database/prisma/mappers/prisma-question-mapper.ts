3e6baa8b31e0079b66b5e4d8f139ba81350c4dfe
André Luiz <silva.ala82@gmail.com>
2024-01-17 20:12:57 -0300

    feat: implements prisma repositoty

--- src/infra/database/prisma/mappers/prisma-question-mapper.ts
+++ src/infra/database/prisma/mappers/prisma-question-mapper.ts
@@ -1,10 +1,10 @@
 import { UniqueEntityId } from '@/core/entities/unique-entity-id'
 import { Question } from '@/domain/forum/enterprise/entities/question'
 import { Slug } from '@/domain/forum/enterprise/entities/value-objects/slug'
-import { Question as PrismaClient } from '@prisma/client'
+import { Question as PrismaQuestion, Prisma } from '@prisma/client'
 
 export class PrismaQuestionMapper {
-  static toDomain(raw: PrismaClient): Question {
+  static toDomain(raw: PrismaQuestion): Question {
     return Question.create(
       {
         title: raw.title,
@@ -19,5 +19,18 @@ export class PrismaQuestionMapper {
       },
       new UniqueEntityId(raw.id),
     )
+  }
+
+  static toPrisma(question: Question): Prisma.QuestionUncheckedCreateInput {
+    return {
+      id: question.id.toString(),
+      authorId: question.authorId.toString(),
+      bestAnswerId: question.bestAnswerId?.toString(),
+      title: question.title,
+      content: question.content,
+      slug: question.slug,
+      createdAt: question.createdAt,
+      updatedAt: question.updatedAt,
+    }
   }
 }
