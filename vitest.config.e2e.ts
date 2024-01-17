bf240d8be71d51f7d9f7f78e77dc93543753f5fa
André Luiz <silva.ala82@gmail.com>
2024-01-17 11:11:23 -0300

    feat: create config for e2e tests

--- vitest.config.e2e.ts
+++ vitest.config.e2e.ts
@@ -12,6 +12,7 @@ export default defineConfig({
     },
     root: './',
     exclude: [...configDefaults.exclude, '**/data/pg/**'],
+    setupFiles: ['./test/setup-e2e.ts'],
   },
   resolve: {
     alias: {
