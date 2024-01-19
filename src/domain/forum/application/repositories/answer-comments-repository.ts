eddad92bc2baf38e5bd81bb0b3f5bef51f7a381f
André Luiz <silva.ala82@gmail.com>
2024-01-19 15:29:25 -0300

    fix: adjust interface for abstrac class usage

--- src/domain/forum/application/repositories/answer-comments-repository.ts
+++ src/domain/forum/application/repositories/answer-comments-repository.ts
@@ -1,11 +1,11 @@
 import { PaginationParams } from '@/core/repositories/pagination-params'
 import { AnswerComment } from '../../enterprise/entities/answer-comment'
 
-export interface AnswersCommentsRepository {
-  create(answer: AnswerComment): Promise<void>
-  delete(answer: AnswerComment): Promise<void>
-  findById(id: string): Promise<AnswerComment | null>
-  findManyByAnswerId(
+export abstract class AnswersCommentsRepository {
+  abstract create(answer: AnswerComment): Promise<void>
+  abstract delete(answer: AnswerComment): Promise<void>
+  abstract findById(id: string): Promise<AnswerComment | null>
+  abstract findManyByAnswerId(
     answerId: string,
     params: PaginationParams,
   ): Promise<AnswerComment[]>
--- src/domain/forum/application/repositories/answers-repository.ts
+++ src/domain/forum/application/repositories/answers-repository.ts
@@ -1,13 +1,14 @@
 import { PaginationParams } from '@/core/repositories/pagination-params'
 import { Answer } from '@/domain/forum/enterprise/entities/answer'
 
-export interface AnswerRepository {
-  findManyByQuestionId(
+export abstract class AnswerRepository {
+  abstract findManyByQuestionId(
     questionId: string,
     params: PaginationParams,
   ): Promise<Answer[]>
-  findById(id: string): Promise<Answer | null>
-  save(answer: Answer): Promise<void>
-  create(answer: Answer): Promise<void>
-  delete(answer: Answer): Promise<void>
+
+  abstract findById(id: string): Promise<Answer | null>
+  abstract save(answer: Answer): Promise<void>
+  abstract create(answer: Answer): Promise<void>
+  abstract delete(answer: Answer): Promise<void>
 }
