45cdc44e3aa0583bcebe9f0fe420517ff6c99a77
André Luiz <silva.ala82@gmail.com>
2024-01-23 11:30:22 -0300

    feat: implemets Redis cache

--- src/infra/cache/cache.module.ts
+++ src/infra/cache/cache.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common'
 import { EnvModule } from '../env/env.module'
-import { EnvService } from '../env/env.service'
+import { CacheRepository } from './cache-repository'
+import { RedisClassRepository } from './redis-cache-repository'
+import { RedisService } from './redis/redis.service'
 
 @Module({
   imports: [EnvModule],
-  providers: [EnvService],
+  providers: [
+    RedisService,
+    {
+      provide: CacheRepository,
+      useClass: RedisClassRepository,
+    },
+  ],
+  exports: [CacheRepository],
 })
 export class CacheModule {}
